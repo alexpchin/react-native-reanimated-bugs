745d97882f69e048b1a6e08aed205ce318cd8668
Alex Chin <alex@alexchin.co.uk>
2021-02-25 01:02:33 +0000

    Creating an example of the bug

--- App.js
+++ App.js
@@ -1,113 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
+  FlatList,
+  ActivityIndicator,
   View,
   Text,
-  StatusBar,
+  // TouchableOpacity,
+  Button,
+  Alert,
 } from 'react-native';
+import Animated from 'react-native-reanimated';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
-const App: () => React$Node = () => {
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb282b',
+    title: 'Fourth Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97as3',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e294f',
+    title: 'Sixth Item',
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53lnasdsd',
+    title: 'Seventh Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fblklmasass3',
+    title: 'Eigth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-14557lnasd4f',
+    title: 'Ninth Item',
+  },
+];
+
+const Item = ({title, onPress}) => {
+  const [deleting, setDeleting] = useState(false);
+
+  const _onPress = () => {
+    setDeleting(true);
+
+    // Calling before to have a delay before state update
+    onPress();
+
+    setTimeout(() => {
+      setDeleting(false);
+    }, 2000);
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <TouchableOpacity onPress={_onPress}>
+        <View style={styles.button}>
+          {deleting ? <ActivityIndicator /> : <Text>Delete</Text>}
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const App = () => {
+  const [data, setData] = useState(DATA);
+
+  const onPress = (id) => {
+    setData(data.filter((d) => d.id !== id));
+  };
+
+  const renderItem = ({item}) => (
+    <Item title={item.title} onPress={() => onPress(item.id)} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button
+        style={styles.button}
+        onPress={() => Alert.alert('Pressed')}
+        title={'Button'}
+      />
+      <AnimatedFlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => String(index)}
+        // This seems to be better
+        // keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    // marginTop: StatusBar.currentHeight || 0,
+    // backgroundColor: 'green',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
+  title: {
+    fontSize: 32,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    width: 100,
+    height: 20,
+    backgroundColor: 'red',
   },
 });
 
